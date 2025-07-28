3631dedd1d6df3f680a94c5cc8d0608b30ef45a3
Lan Gitto <git762647@gmail.com>
2025-07-28 10:04:32 +0800

    done home page

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,9 +1,9 @@
 const Header = () => {
   const listStyle = "hover:scale-110 transition-transform duration-100 ";
   return (
-    <header className="bg-[#CAE8BD] h-[100px] flex  justify-center">
-      <nav className=" flex items-center ">
-        <ul className=" text-2xl flex flex-row w-[600px] justify-between font-light cursor-pointer ">
+    <header className="bg-[#CAE8BD] h-[180px] sm:h-[100px] flex  justify-center  overflow-hidden ">
+      <nav className="  flex items-center  ">
+        <ul className=" text-2xl flex flex-col w-[600px]  items-center font-light cursor-pointer sm:flex-row sm:justify-between gap-2  ">
           <li className={listStyle}>Home</li>
           <li className={listStyle}>About</li>
           <li className={listStyle}>Project</li>
--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,23 +1,31 @@
 const Home = () => {
   return (
     <>
-      <div>
-        <section>
-          <img src="" alt="" srcset="" />
-          <div>
-            <h1 className="text-7xl font-bold">
+        <section className="flex flex-col items-center px-5 py-7  min-h-screen justify-around ">
+             <div className="flex flex-col items-center gap-5 md:flex-col lg:flex-row " >
+          <img src="https://cdn-icons-png.flaticon.com/512/147/147142.png" alt="Picture" className="lg:w-[400px] lg:h-[380px] w-[300px] " />
+          <div className="flex flex-col justify-between  h-[380px] items-center mb-5">
+            <h1 className="lg:text-6xl text-5xl font-bold font-mono">
               Hi, Allan S. <span className="text-[#B0DB9C]">Aranda III</span>
             </h1>
-            <div>
-              I'm a 19-year-old BSIT 3rd year student from Sta. Barbara,
+            <div className="bg-[#CAE8BD] px-5 py-5 w-full  lg:h-[250px] h-[200px] md:h-[250px] flex justify-center items-center md:w-full  ">
+              <h1 className="lg:text-3xl text-center leading-snug font-light t md:text-2xl">
+               I'm a 19-year-old BSIT 3rd year student from Sta. Barbara,
               Pangasinan. I'm interested in coding, and I want to improve my
-              critical thinking and problem-solving skills.
+              critical thinking and problem-solving skills. </h1>
             </div>
+        
           </div>
-          “Aspiring Full-Stack Developer | BSIT Student | Passionate about Web
-          Development.”
+          
+          
+             </div>
+    
+             <h1 className="text-2xl text-center leading-snug font-mono lg:text-5xl ">
+          “Aspiring Full-Stack Developer | BSIT Student | <span className="text-[#B0DB9C]">Passionate about Web
+          Development</span>.” </h1>
+             
         </section>
-      </div>
+
     </>
   );
 };
